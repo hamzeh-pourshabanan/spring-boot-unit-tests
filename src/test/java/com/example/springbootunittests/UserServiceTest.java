83b97f9843f215687061d1ea59a9e096f30cfbe2
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2023-09-16 01:28:09 +0330

    test: Test get user

---
+++ src/test/java/com/example/springbootunittests/UserServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.springbootunittests;
+
+import com.example.springbootunittests.service.UserService;
+import jakarta.annotation.Resource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {SpringBootUnitTestsApplication.class})
+public class UserServiceTest {
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void testGetUser() {
+        Assert.assertEquals(userService.getUser(), "Tom");
+    }
+}
